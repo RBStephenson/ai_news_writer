40607928fdd9390f958591431eef4dee0d86ec71
brent <brentstephenson@hotmail.com>
2019-10-18 22:39:45 -0400

    Researching and creating a list of AI related feeds to start with.

--- ai_news_writer/ai_news_writer.py
+++ ai_news_writer/ai_news_writer.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+# TODO: Need to create a data structure and store the following items in a file/database
+# ai_trends_feed = "https://www.aitrends.com/feed/"
+# science_daily_feed = "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml"
+machine_learning_mastery_feed = "http://machinelearningmastery.com/blog/feed/"
+mit_feed = "http://news.mit.edu/rss/topic/artificial-intelligence2"
+reddit_artificial_feed = "https://www.reddit.com/r/artificial/.rss"
+chatbot_mag_feed = "https://chatbotsmagazine.com/feed"
+chatbots_life_feed = "https://chatbotslife.com/feed"
+aws_feed = "https://aws.amazon.com/blogs/machine-learning/feed/"
+ibm_developer_feed = "https://developer.ibm.com/patterns/category/artificial-intelligence/feed/"
+lex_fridman_feed = "https://lexfridman.com/category/ai/feed/"
+reddit_singularity_feed = "https://www.reddit.com/r/singularity/.rss?format=xml"
+medium_archie_feed = "https://medium.com/feed/archieai"
+nvidia_feed = "http://feeds.feedburner.com/nvidiablog"
+
+feeds = [
+    {"name": "AI Trends", "url": "https://www.aitrends.com/feed/"},
+    {"name": "Science Daily", "url": "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml"}
+]
