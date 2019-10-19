73b6343c9199d9e1388d01b1eb51a246e53108bf
brent <brentstephenson@hotmail.com>
2019-10-19 07:23:10 -0400

    Trying to get a handle on creating unit tests with pytest. Additionally, I am creating the initial feed and collecting the sources I will use.

--- ai_news_writer/ai_news_writer.py
+++ ai_news_writer/ai_news_writer.py
@@ -2,22 +2,28 @@
 
 """Main module."""
 
-# TODO: Need to create a data structure and store the following items in a file/database
-# ai_trends_feed = "https://www.aitrends.com/feed/"
-# science_daily_feed = "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml"
-machine_learning_mastery_feed = "http://machinelearningmastery.com/blog/feed/"
-mit_feed = "http://news.mit.edu/rss/topic/artificial-intelligence2"
-reddit_artificial_feed = "https://www.reddit.com/r/artificial/.rss"
-chatbot_mag_feed = "https://chatbotsmagazine.com/feed"
-chatbots_life_feed = "https://chatbotslife.com/feed"
-aws_feed = "https://aws.amazon.com/blogs/machine-learning/feed/"
-ibm_developer_feed = "https://developer.ibm.com/patterns/category/artificial-intelligence/feed/"
-lex_fridman_feed = "https://lexfridman.com/category/ai/feed/"
-reddit_singularity_feed = "https://www.reddit.com/r/singularity/.rss?format=xml"
-medium_archie_feed = "https://medium.com/feed/archieai"
-nvidia_feed = "http://feeds.feedburner.com/nvidiablog"
-
-feeds = [
+ai_news_feeds = [
     {"name": "AI Trends", "url": "https://www.aitrends.com/feed/"},
-    {"name": "Science Daily", "url": "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml"}
+    {"name": "Science Daily", "url": "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml"},
+    {"name": "MIT News - Artificial Intelligence", "url": "http://news.mit.edu/rss/topic/artificial-intelligence2"},
+    {"name": "r/artificial", "url": "https://www.reddit.com/r/artificial/.rss"},
+    {"name": "Chatbots Magazine - Medium", "url": "https://chatbotsmagazine.com/feed"},
+    {"name": "Towards Data Science - Medium", "url": "https://towardsdatascience.com/feed"},
+    {"name": "Chatbots Life - Medium", "url": "https://chatbotslife.com/feed"},
+    {"name": "AWS Machine Learning Blog", "url": "https://aws.amazon.com/blogs/machine-learning/feed/"},
+    {"name": "Artificial Intelligence - IBM Developer",
+     "url": "https://developer.ibm.com/patterns/category/artificial-intelligence/feed/"},
+    {"name": "Lex Fridman - Artificial Intelligence (AI)", "url": "https://lexfridman.com/category/ai/feed/"},
+    {"name": "r/singularity", "url": "https://www.reddit.com/r/singularity/.rss?format=xml"},
+    {"name": "Archie.AI - Medium", "url": "https://medium.com/feed/archieai"},
+    {"name": "The Official NVIDIA Blog", "url": "http://feeds.feedburner.com/nvidiablog"},
+    {"name": "OpenAI Blog", "url": "https://openai.com/blog/rss/"},
+    {"name": "VentureBeat", "url": "http://feeds.feedburner.com/venturebeat/SZYF?format=xml"},
 ]
+
+import feedparser
+
+
+def parse_feed(name, url):
+    output = feedparser.parse(url_file_stream_or_string=url)
+    print(output)
--- tests/test_ai_news_writer.py
+++ tests/test_ai_news_writer.py
@@ -5,21 +5,28 @@
 
 import pytest
 
-
 from ai_news_writer import ai_news_writer
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def ai_news_feeds():
+    ai_feeds = ai_news_writer.ai_news_feeds
+    return ai_feeds
 
 
-def test_content(response):
+@pytest.fixture
+def parse_feed():
+    feed = ai_news_writer.ai_news_feeds[0]
+    return ai_news_writer.parse_feed(feed['name'], feed['url'])
+
+
+def test_content(ai_news_feeds):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert len(ai_news_feeds) > 0
+
+
+def test_feed_parse(parse_feed):
+    print(parse_feed)
+    assert parse_feed is None
