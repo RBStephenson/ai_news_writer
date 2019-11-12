0a53d703f1d6a1c93366359088ce16f657beae90
brent <brentstephenson@hotmail.com>
2019-11-12 06:45:11 -0500

    Continuing to implement the rss_news_feed class and unit tests.

--- ai_news_writer/rss_news_feed.py
+++ ai_news_writer/rss_news_feed.py
@@ -4,3 +4,42 @@ class RSSNewsFeed(object):
 
     def add_item(self, news_feed, feed_name, category):
         self.news_feeds.append({"url": news_feed, "name": feed_name, "category": category})
+
+    def remove_item(self, url):
+        # I'm not sure I like this method or not, which makes me think I need to
+        # change the storage method too. I know that I want to eventually store
+        # this in a database, so I might investigate that. Originally, I wanted
+        # to use a pandas DataFrame so I could pass that to SQLAlchemy and
+        # handle everything that way. I will settle for this right now, just to
+        # keep it as simple as possible.
+        self.news_feeds = [x for x in self.news_feeds if x['url'] != url]
+
+    def update_item(self, key, url=None, name=None, category=None):
+        # This method is a real kludge and I don't like it at all. I know that I could be doing something
+        # much more efficient Python-wise, however, I don't know what that is right now. I also realize that
+        # I still have a difficult time grasping list comprehensions, how they work and what they return.
+        # I had originally had a list comprehension to return the `org_item` but it was not returning the
+        # item I expected. I found the below `for` loop returns what I wanted so I could perform the update.
+        org_item = None
+
+        for item in iter(self.news_feeds):
+            if item['url'] == key:
+                org_item = item
+                break
+
+        self.remove_item(key)
+
+        new_item = {"url": url, "name": name, "category": category}
+        if new_item['url'] is None:
+            new_item['url'] = org_item['url']
+
+        if new_item['name'] is None:
+            new_item['name'] = org_item['name']
+
+        if new_item['category'] is None:
+            new_item['category'] = org_item['category']
+
+        self.news_feeds.append(new_item)
+
+    def get_item(self, key):
+        return [item for item in self.news_feeds if item['url'] == key][0]
--- tests/test_rss_news_feeds.py
+++ tests/test_rss_news_feeds.py
@@ -8,15 +8,34 @@ class RSSNewsFeedsUnitTests(unittest.TestCase):
         obj_under_test = rss_news_feed.RSSNewsFeed()
         obj_under_test.add_item('http://www.mynewsfeed.com', 'My News Feed', 'Test Category')
         self.assertTrue(obj_under_test.news_feeds[0]['url'] == 'http://www.mynewsfeed.com')
+        print(obj_under_test.news_feeds)
 
     def test_update_rss_news_feed(self):
-        pass
+        obj_under_test = rss_news_feed.RSSNewsFeed()
+        obj_under_test.add_item('http://www.mynewsfeed.com', 'My News Feed', 'Test Category')
+        obj_under_test.update_item(key='http://www.mynewsfeed.com', url='http://www.mynewsfeed2.com')
+        self.assertTrue(obj_under_test.news_feeds[0]['url'] != 'http://www.mynewsfeed.com')
+        self.assertTrue(obj_under_test.news_feeds[0]['url'] == 'http://www.mynewsfeed2.com')
+        print(obj_under_test.news_feeds)
 
     def test_delete_rss_news_feed(self):
-        pass
+        obj_under_test = rss_news_feed.RSSNewsFeed()
+        obj_under_test.add_item('http://www.mynewsfeed.com', 'My News Feed', 'Test Category')
+        self.assertTrue(obj_under_test.news_feeds[0]['url'] == 'http://www.mynewsfeed.com')
+        obj_under_test.remove_item('http://www.mynewsfeed.com')
+        self.assertTrue(len(obj_under_test.news_feeds) == 0)
+        print(obj_under_test.news_feeds)
 
     def test_get_rss_news_feed_collection(self):
-        pass
+        obj_under_test = rss_news_feed.RSSNewsFeed()
+        obj_under_test.add_item('http://www.mynewsfeed.com', 'My News Feed', 'Test Category')
+        obj_under_test.add_item('http://www.notmynewsfeed.com', 'Not My News Feed', 'Test Category')
+        obj_under_test.add_item('http://www.anothernewsfeed.com', 'Another News Feed', 'Test Category')
+
+        expected_item = obj_under_test.news_feeds[1]
+        actual_item = obj_under_test.get_item('http://www.notmynewsfeed.com')
+
+        self.assertEqual(expected_item, actual_item)
 
 
 if __name__ == '__main__':
